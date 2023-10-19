54818ce672ea0ea735b3db13549261eb6d3bda51
Arystos <arystotelos@gmail.com>
2023-10-19 16:37:24 +0200

    Corrected Player inputs
    Remember that plater Layer is at index 8, if you need ui layer set it above

--- Team-4-Project/Assets/Scripts/PlayerMovement.cs
+++ Team-4-Project/Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,17 @@ public class PlayerMovement : MonoBehaviour
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+
+            // Bit shift the index of the layer (8) to get a bit mask
+            int layerMask = 1 << 8;
+            // This would cast rays only against colliders in layer 8.
+            // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+            layerMask = ~layerMask;
+
             if (Physics.Raycast(ray, out hit))
             {
 
-                if (hit.collider.CompareTag("Square")) // Remember the tag
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)) // Remember the tag
                 {
                     SquareStatus squareStatus = hit.collider.GetComponent<SquareStatus>();
                     if (squareStatus != null && !squareStatus.isOccupied)
