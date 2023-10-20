7b55e92165107f4ebe8d336156c707be8e25f426
Arystos <arystotelos@gmail.com>
2023-10-20 15:54:49 +0200

    GameStateManager Persists between Scenes

--- Team-4-Project/Assets/Scripts/GameStateManager.cs
+++ Team-4-Project/Assets/Scripts/GameStateManager.cs
@@ -2,13 +2,25 @@ using UnityEngine;
 
 public class GameStateManager : MonoBehaviour
 {
-    public enum GameState { GameStateStart, GameStatePlaying, GameStatePaused, GameStateEnd }
+    public enum GameState { 
+        GameStateStart,     // Show Main Menu
+        GameStatePlaying,   // Player's turn
+        GameStateEnemyTurn, // Enemy's turn
+        GameStatePaused,    // Show the Pause Menu
+        GameStateEnd }      // Game Over / Victory!
+
     public GameState CurrentState { get; private set; } = GameState.GameStateStart;
 
     private PlayerMovement playerMovement;
 
     private void Awake()
     {
+        // Only 1 instance of GameStateManager exists between scenes, and is not destroyable
+        if (FindObjectsOfType<GameStateManager>().Length > 1) 
+            Destroy(gameObject);
+        else
+            DontDestroyOnLoad(gameObject);
+
         playerMovement = FindObjectOfType<PlayerMovement>();
         OnGameStateChanged += HandleGameStateChange;
     }
