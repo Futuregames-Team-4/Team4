9fd120ae053df59fc1a766f353e4cd96488f29e9
FelleL <felix.lind@edu.futuregames.nu>
2023-10-20 13:36:03 +0200

    Inventory and items

---
+++ Team-4-Project/Assets/Scripts/inventoryAndItems/ItemInstance.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class ItemInstance : MonoBehaviour
+{
+    public ItemData itemType;
+    public int condition;
+    public int ammo;
+}
